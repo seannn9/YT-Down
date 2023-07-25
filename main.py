e1d5c32a822872aa99a7bfc6e93aae8e0fd3f96d
seannn9 <naeskirlu9@gmail.com>
2023-07-25 13:18:05 +0800

    basic downloader

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from pytube import YouTube
 
+down_path = "/Users/seanulric/Downloads"
 link = input("Video Link: ")
 yt = YouTube(link)
 print(yt.title)
-resol = input("Resolution: ")
-print(yt.streams.filter(file_extension="mp4", resolution=resol)) 
-itag = input("Itag: ")
-stream = yt.streams.get_by_itag(itag)
-stream.download()
+highest = int(input("Highest res or itag (1 or 2): "))
+if highest == 1:
+    stream = yt.streams.get_highest_resolution()
+    stream.download(down_path)
+else:
+    resol = input("Resolution: ")
+    print(yt.streams.filter(file_extension="mp4", res=resol)) 
+    itag = input("Itag: ")
+    stream = yt.streams.get_by_itag(itag)
+    stream.download(down_path)
